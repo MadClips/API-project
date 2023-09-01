c11ff0b4816ff322ad7c404c13b66e8133ec530d
wayne <wayneluejr@gmail.com>
2023-09-01 18:24:08 -0400

    put "options" as first argument for both removeColumns for first and lastName migrations

--- backend/db/migrations/20230901040439-add-firstName-to-users.js
+++ backend/db/migrations/20230901040439-add-firstName-to-users.js
@@ -14,6 +14,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn(`Users`, `firstName`);
+    await queryInterface.removeColumn(options, `firstName`);
   },
 };
--- backend/db/migrations/20230901041026-add-lastName-to-users.js
+++ backend/db/migrations/20230901041026-add-lastName-to-users.js
@@ -14,6 +14,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn(`Users`, `lastName`);
+    await queryInterface.removeColumn(options, `lastName`);
   },
 };
