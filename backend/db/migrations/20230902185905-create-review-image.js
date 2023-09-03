460ee9c3631a6d2434e99fdf06a76e0d14a5e38a
wayne <wayneluejr@gmail.com>
2023-09-03 00:25:38 -0400

    created association between reviews and review images

--- backend/db/migrations/20230902185905-create-review-image.js
+++ backend/db/migrations/20230902185905-create-review-image.js
@@ -1,31 +1,34 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('ReviewImages', {
+    await queryInterface.createTable("ReviewImages", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       reviewId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: { model: `Reviews` },
+        onDelete: `CASCADE`,
+        hooks: true,
       },
       url: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages');
-  }
+    await queryInterface.dropTable("ReviewImages");
+  },
 };
