677c56eb1d7078bd620e3532c0b9ea64f266855f
wayne <wayneluejr@gmail.com>
2023-09-06 13:15:32 -0400

    added the options object to the rest of the migration files

--- backend/db/migrations/20230902180640-create-booking.js
+++ backend/db/migrations/20230902180640-create-booking.js
@@ -1,43 +1,54 @@
 "use strict";
-/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+options.tableName = "Bookings";
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Bookings", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      "Bookings",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          references: { model: `Spots` },
+          onDelete: `CASCADE`,
+          hooks: true,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          references: { model: `Users` },
+          onDelete: `CASCADE`,
+          hooks: true,
+        },
+        startDate: {
+          type: Sequelize.DATE,
+        },
+        endDate: {
+          type: Sequelize.DATE,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      spotId: {
-        type: Sequelize.INTEGER,
-        references: { model: `Spots` },
-        onDelete: `CASCADE`,
-        hooks: true,
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        references: { model: `Users` },
-        onDelete: `CASCADE`,
-        hooks: true,
-      },
-      startDate: {
-        type: Sequelize.DATE,
-      },
-      endDate: {
-        type: Sequelize.DATE,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Bookings");
+    await queryInterface.dropTable(options);
   },
 };
