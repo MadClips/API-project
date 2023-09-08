b36f24343d7bf39bb97e0945495d71199d219324
wayne <wayneluejr@gmail.com>
2023-09-08 01:30:39 -0400

    finished local test for spotId bookings route

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -8,6 +8,7 @@ const {
   Review,
   User,
   ReviewImage,
+  Booking,
   sequelize,
 } = require(`../../db/models`);
 
@@ -54,6 +55,20 @@ router.get(`/current`, requireAuth, async (req, res, _next) => {
     });
   });
   return res.status(200).json({ Spots: listOfSpots });
+});
+
+//* GET BOOKINGS BY SPOT ID
+
+router.get(`/:spotId/bookings`, async (req, res, next) => {
+  const spot = await Spot.findByPk(req.params.spotId);
+  if (!spot) {
+    return res.status(404).json({ message: `Spot couldn't be found` });
+  }
+  const bookings = await Booking.findAll({
+    where: { spotId: req.params.spotId },
+    include: [{ model: User, attributes: [`id`, `firstName`, `lastName`] }],
+  });
+  res.status(200).json({ Bookings: bookings });
 });
 
 //* GET REVIEWS BY SPOT ID
